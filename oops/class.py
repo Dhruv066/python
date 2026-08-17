7f5de7ed876e084a436a1ec8ffe980921e4d876b
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-08-17 19:13:31 +0530

    class and constructor

---
+++ oops/class.py
@@ -0,0 +1,7 @@
+class college:
+    name = "biet"
+    add ="lucnow"
+#  objects creation
+
+c1 = college()
+print(c1.name)
---
+++ oops/constructor.py
@@ -0,0 +1,5 @@
+class animal :
+    def __init__(self,name ):
+        print(name ," is a living being." )
+
+dog = animal("choco")
