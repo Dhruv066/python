f21a09d133958e9956a52c55034eb567b61ba366
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-08-31 23:32:55 +0530

    query selector

--- Pandas/querySelector.py
+++ Pandas/querySelector.py
@@ -2,4 +2,5 @@ import pandas as pd
 df= pd.read_csv("F:\python\Pandas\globalAirQuality.csv")
 f= pd.DataFrame(df)
 print(f.columns)
-print(f.query("temperature>30 & aqi>100 & city=='New York'")[["city","temperature"]])
+aqiVal= 25
+print(f.query("temperature>@aqiVal & aqi>100 & city=='New York'")[["city","temperature"]])
