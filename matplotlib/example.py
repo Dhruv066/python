9607a9006eb35c28bef53d060589bafef4569a26
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-09-15 14:11:05 +0530

    eample practice

---
+++ matplotlib/example.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+x=[1,9,6,7,5]
+y=[5,9,36,74,89]
+plt.plot(x,y,color="red",linestyle=":",linewidth=5,marker="^",markersize=6)
+plt.xlabel("X- axis label")
+plt.ylabel("Y- axis label")
+plt.title("example of matplotlib ")
+# plt.grid()
+
+plt.show()
